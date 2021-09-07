2f9a9923fe96f7adfea9abbc0696a6778144646b
mathias-tad <mathiasstadesse@gmail.com>
2021-09-07 15:12:49 +0000

    Print aphabet ecept e and q

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 for char in range(ord("a"), ord("z") + 1):
-    print("{}".format(chr(char)), end="")
-    if char == ord("e") or char == ord("q"):
-        print("\b", end="")
+    if char != ord("e") and char != ord("q"):
+        print("{}".format(chr(char)), end="")
