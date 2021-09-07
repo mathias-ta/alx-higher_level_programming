7ff95f8d317e8098b198e8bfb0b393a7a843acdf
mathias-tad <mathiasstadesse@gmail.com>
2021-09-07 14:21:54 +0000

    Fid last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,8 +8,8 @@ else:
 	rem = number % (-10)
 
 if rem > 5:
-	print("Last digit of {:d} is {:d} and is greater then 5".format(number, rem))
-if rem == 0:
-	print("Last digit of {:d} is {:d} and is 0".format(number, rem))
-if rem < 6 and rem != 0:
-	print("last digit of {:d} is {:d} and is less than 6 and not 0".format(number, rem))
+	print("Last digit of {} is {} and is greater than 5".format(number, rem))
+elif rem == 0:
+	print("Last digit of {} is {} and is 0".format(number, rem))
+else:
+	print("Last digit of {} is {} and is less than 6 and not 0".format(number, rem))
