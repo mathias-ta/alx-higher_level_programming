5818ed0b32321ed2227e5993a1651052fb092dbe
mathias-tad <mathiasstadesse@gmail.com>
2021-09-07 14:38:49 +0000

    Fid last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+str = "Last digit of "
 
 if number >= 0:
-	rem = number % 10
+    rem = number % 10
 else:
-	rem = number % (-10)
+    rem = number % (-10)
 
 if rem > 5:
-	print("Last digit of {} is {} and is greater than 5"
-		.format(number, rem))
+    print(str + "{} is {} and is greater than 5".format(number, rem))
 elif rem == 0:
-	print("Last digit of {} is {} and is 0"
-		.format(number, rem))
+    print(str + "{} is {} and is 0".format(number, rem))
 else:
-	print("Last digit of {} is {} and is less than 6 and not 0"
-		.format(number, rem))
+    print(str + "{} is {} and is less than 6 and not 0".format(number, rem))
