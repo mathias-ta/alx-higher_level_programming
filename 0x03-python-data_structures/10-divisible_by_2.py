0fc6927958831a8b61dc1a7b58d48563f222707d
mathias-tad <mathiasstadesse@gmail.com>
2021-09-13 16:44:48 -0700

    Find even

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     j = 0
-    new_list = my_list.copy() 
+    new_list = my_list.copy()
     for i in my_list:
         if (i % 2) == 0:
             new_list[j] = True
