31cef8bc702f10befc424eed0b4881ffed865007
mathias-tad <mathiasstadesse@gmail.com>
2021-09-13 16:14:02 -0700

    Multiple returns

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    leng = len(sentence)
+    if leng == 0:
+        char = None
+    else:
+        for char in sentence:
+             return leng, char
+    return leng, char
