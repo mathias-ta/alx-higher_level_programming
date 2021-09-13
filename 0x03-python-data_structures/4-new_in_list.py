57f453412eda09b9da360ec88ce19fa445bffe01
mathias-tad <mathiasstadesse@gmail.com>
2021-09-13 14:17:58 -0700

    Copy without changin the first

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
-    new_list = my_list
+def new_in_list(my_list, idx, element):
+    new_list = my_list.copy()
     if idx < 0 or idx >= len(my_list):
         return new_list
     new_list[idx] = element
     return new_list
+
+my_list = [1, 2, 3, 4, 5]
+idx = 3
+new_element = 9
+new_list = new_in_list(my_list, idx, new_element)
+print(new_list)
+print(my_list)
