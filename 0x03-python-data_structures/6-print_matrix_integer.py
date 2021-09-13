5c0eb5bdcc1fd8f4b58ab2c98ba18e5a8ca49ae8
mathias-tad <mathiasstadesse@gmail.com>
2021-09-13 15:30:26 -0700

    Print a matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
-        for j in i):
+        for j in i:
             if k is not i[len(matrix) - 1]:
                 print("{:d}".format(k), end=" ")
             else:
