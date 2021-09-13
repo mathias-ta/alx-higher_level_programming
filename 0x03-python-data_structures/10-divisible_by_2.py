ffee7c46f593cf6725e84f80f774fba8a2a4a5c7
mathias-tad <mathiasstadesse@gmail.com>
2021-09-13 16:43:13 -0700

    Find even

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    j = 0
+    new_list = my_list.copy() 
+    for i in my_list:
+        if (i % 2) == 0:
+            new_list[j] = True
+        else:
+            new_list[j] = False
+        j = j + 1
+    return new_list
