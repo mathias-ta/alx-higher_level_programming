cac7968c6cd8d936b6fe3c299b67c9874354bd56
mathias-tad <mathiasstadesse@gmail.com>
2021-09-13 14:49:56 -0700

    Print a string without c

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    charr = "" 
+    i = 0
+    for char in my_string:
+        if ord(char) == ord('c') or ord(char) == ord('C'):
+            char = ""
+        charr = charr + char
+    return charr
+
+print(no_c("Holberton School"))
