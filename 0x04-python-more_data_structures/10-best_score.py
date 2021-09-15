e9390e5c29bd98673161fd08fd91def9f1199a3f
mathias-tad <mathiasstadesse@gmail.com>
2021-09-15 16:24:26 -0700

    Find the highest scorer

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    highest_value = 0
-    if a_dictionary is not None:
-        for key, value in sorted(a_dictionary.items()):
-            if value >= highest_value:
-                highest_value = value
-        return key
-    return None
+    if a_dictionary:
+        return max(a_dictionary, a_dictionary.get(key))
