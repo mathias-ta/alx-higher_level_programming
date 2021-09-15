8e70247df5355ddde6568fa4fdf2c5c4be40df6a
mathias-tad <mathiasstadesse@gmail.com>
2021-09-15 15:56:42 -0700

    Delete a key from a dictionary

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    a_dictionary.pop(key)
-    return a_dictionary
+    if a_dictionary is not None:
+        if key in a_dictionary:
+            a_dictionary.pop(key)
+        return a_dictionary
