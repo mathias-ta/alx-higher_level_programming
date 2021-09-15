97889392d6313ffd5f6c03d3f4629b897054dedb
mathias-tad <mathiasstadesse@gmail.com>
2021-09-15 16:19:52 -0700

    Find the highest scorer

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    highest_value = 0
+    if a_dictionary is not None:
+        for key, value in sorted(a_dictionary.items()):
+            if value >= highest_value:
+                highest_value = value
+        return key
