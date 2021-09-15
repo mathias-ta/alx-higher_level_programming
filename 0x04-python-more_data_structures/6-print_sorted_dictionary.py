9459fe6436fcbd975412342f080da5462784c649
mathias-tad <mathiasstadesse@gmail.com>
2021-09-15 15:35:25 -0700

    Print a sorted dictionary

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    print(f"Number: {a_dictionary.get('Number')}")
-    print(f"ids: {a_dictionary.get('ids')}")
-    print(f"language: {a_dictionary.get('language')}")
-    print(f"track: {a_dictionary.get('track')}")
+    for key, value in sorted(a_dictionary.items()):
+        print(f"{key}: {value}")
