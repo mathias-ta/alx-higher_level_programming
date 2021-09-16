f132b7b68ad939f1451e2bf8f3fd845a63c30347
mathias-tad <mathiasstadesse@gmail.com>
2021-09-15 17:52:39 -0700

    Calculate average weight score

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if my_list is not None:
+        total_score = 0
+        total_weight = 0
+        av_score = 0
+        for tupl in my_list:
+            (score, weight) = tupl
+            total_score += (score * weight)
+            total_weight += weight
+        if total_weight > 0:
+            av_score = total_score/total_weight
+    else:
+        return (0)
