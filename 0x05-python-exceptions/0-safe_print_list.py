85f17c24f916d4541e67274ed66337fecb24a28b
mathias-tad <mathiasstadesse@gmail.com>
2021-09-27 10:41:24 -0700

    Safe print

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    for i in range(0, x):
+    for i in range(0, x - 1):
         try:
             print("{}".format(my_list[i]), end="")
         except IndexError:
