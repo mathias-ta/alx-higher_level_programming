8b4a94980bbc8725201a8081777e6b240d455970
mathias-tad <mathiasstadesse@gmail.com>
2021-09-28 15:56:01 -0700

    Empty class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """
-Module 1-square
-Defines class Square with private attribute size
+Module 0-square
+Defines class Square
 """
 
 
 class Square:
+    """Empty class"""
     pass
