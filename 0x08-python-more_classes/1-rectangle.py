f900f0ee722faa967cab0d75803e7a0cf6381287
mathias-tad <mathiasstadesse@gmail.com>
2021-10-04 09:05:52 -0700

    Define a rectangle with width and height attributes

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -11,11 +11,25 @@ class Rectangle():
     width and heigh attributes
     """
     def __init__(self, width=0, height=0):
-        if type(width) is not int:
+        self.width = width
+        self.height = height
+
+    def width(self):
+        return self.__width
+
+    def width(self, value):
+        if type(value) is not int:
             raise TypeError("width must be an integer")
-        if width < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
-        if type(height) is not int:
+        self.__width = value
+
+    def height(self):
+        return self.__height
+
+    def height(self, value):
+        if type(value) is not int:
             raise TypeError("height must be an integer")
-        if height < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
+        self.__height = value
