22abd6c4529cb6301cd24ce52a8149baa8373f4b
mathias-tad <mathiasstadesse@gmail.com>
2021-10-04 11:15:05 -0700

    Count instances

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -11,6 +11,7 @@ class Rectangle():
     width and heigh attributes
     """
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
