fb8ed1b10090b4a622e3f8b147574802359496c1
mathias-tad <mathiasstadesse@gmail.com>
2021-10-05 22:40:29 -0700

    Test driven

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,9 +5,12 @@ Method to add two integers
 
 
 def add_integer(a, b=98):
-    if type(a) is not int or type(a) is not float:
+    """
+    Returns sum of a and b
+    """
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    elif type(b) is not int or type(b) is not float:
+    elif not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     else:
         a = int(a)
