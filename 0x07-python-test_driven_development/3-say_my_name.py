719c3262a3e350e29c4848e076924db876e8854f
mathias-tad <mathiasstadesse@gmail.com>
2021-10-05 23:08:11 -0700

    Test driven

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -10,6 +10,6 @@ def say_my_name(first_name, last_name=""):
     """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
-    if not (last_name, str):
+    if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
     print("My name is {} {}".format(first_name, last_name))
