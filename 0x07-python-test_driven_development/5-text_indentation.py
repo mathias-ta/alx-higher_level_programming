b644dda10574ce88e1445852db424ea04e1ab385
mathias-tad <mathiasstadesse@gmail.com>
2021-10-06 00:32:59 -0700

    Test driven

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -8,13 +8,20 @@ def text_indentation(text):
     """
     Print separated string after '.', '?' and ':'
     """
+    ch_char = "a"
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     for char in text:
         if (ord(char) == ord(".") or ord(char) == ord("?") or
                 ord(char) == ord(":")):
+            ch_char = char
             print(char)
             print("")
+        elif ((ord(ch_char) == ord(".") or ord(ch_char) == ord("?") or
+                ord(ch_char) == ord(":")) and ord(char) == ord(" ")):
+            if ord(ch_char) != ord("a"):
+                ch_char = "a"
         else:
+            ch_char = "a"
             print(char, end="")
-# text_indentation(None)
+text_indentation("Holberton. School? Jhon")
