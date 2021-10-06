2dd8b6d4b981e340d1b28b0c3e0749cc6f0ad2e5
mathias-tad <mathiasstadesse@gmail.com>
2021-10-05 23:13:55 -0700

    Test driven

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -8,7 +8,7 @@ def say_my_name(first_name, last_name=""):
     """
     Prints My name is <first name> <last name
     """
-    if isinstance(first_name, str) isinstance(last_name, str):
+    if isinstance(first_name, str) or isinstance(last_name, str):
         print("My name is {} {}".format(first_name, last_name))
     else:
         if not isinstance(first_name, str):
