91133e74a9dee533f7677a391b97ca696406b9b9
mathias-tad <mathiasstadesse@gmail.com>
2021-10-11 15:41:55 -0700

    List object

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+"""
+contains method called lookup that returns of available
+attributes and methods of an object
+"""
+
+
 def lookup(obj):
+    # Method for list objet
     return dir(obj)
