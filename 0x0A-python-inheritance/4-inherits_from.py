447136e9717c200dca4529ee59e172b8f4674557
mathias-tad <mathiasstadesse@gmail.com>
2021-10-11 16:46:13 -0700

    Check for obj is a subclass

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -8,5 +8,6 @@ from the specified class ; otherwise False
 
 
 def inherits_from(obj, a_class):
-    """Check obj for a_class"""
+    """Check obj for a_class
+    """
     return (type(obj) is not a_class and issubclass(type(obj), a_class))
