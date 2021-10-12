04c79c907f4e4ac18ac4af9581e3bc06172391cc
mathias-tad <mathiasstadesse@gmail.com>
2021-10-11 18:09:06 -0700

    Define area

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+contains class called Rectangle that inherits from BaseGeometry
+"""
+
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Inherits from geometry"""
+    def __init__(self, width, height):
+        """
+        Validate and initialize width and height
+        """
+        super().integer_validator("width", width)
+        self.__width = width
+        super().integer_validator("height", height)
+        self.__height = height
+
+    def area(self, width, height):
+        return width * height
+
+    def __str__(self):
+        return ("[Rectangle] {:d}/{:d}".format(width, height))
