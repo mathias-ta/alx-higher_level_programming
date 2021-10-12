089aa1699a54cebfcb460ec51421e4c1d92db2f6
mathias-tad <mathiasstadesse@gmail.com>
2021-10-12 16:09:15 -0700

    from json

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+contains method to change from JSON format
+"""
+
+
+def from_json_string(my_str):
+    """Change my_str from JSON"""
+    import json
+    return json.loads(my_obj)
