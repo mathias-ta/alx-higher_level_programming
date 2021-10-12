0f007c76a51c252acbd7a793c2ad9c2c0b8be91c
mathias-tad <mathiasstadesse@gmail.com>
2021-10-12 15:52:31 -0700

    Write to a file

---
+++ 0x0B-python-input_output/3-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+contains method to write to a file
+"""
+
+
+def read_file(filename=""):
+    """Write text to filename"""
+    with open(filename, mode="r", encoding="utf-8") as f_name:
+        return(f_name.write(text))
