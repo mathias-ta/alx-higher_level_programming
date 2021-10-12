75cb2d0e91c719b6fe2159024064b720fcbf3d42
mathias-tad <mathiasstadesse@gmail.com>
2021-10-11 17:02:02 -0700

    area is not implemented

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Contains class called BaseGeometry which contains method
+called area
+"""
+
+
+class BaseGeometry():
+    """
+    Contains method area
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
