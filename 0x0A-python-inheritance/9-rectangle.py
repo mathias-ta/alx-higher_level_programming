d0562fc1477d2b4c6c9de326ede3542830fef6b4
mathias-tad <mathiasstadesse@gmail.com>
2021-10-11 18:13:06 -0700

    Define area

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -18,7 +18,7 @@ class Rectangle(BaseGeometry):
         super().integer_validator("height", height)
         self.__height = height
 
-    def area(self, width, height):
+    def area(self):
         return self.__width * self.__height
 
     def __str__(self):
