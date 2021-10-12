ecf24776511fdd12a5e67dac86bc26ac22412a6b
mathias-tad <mathiasstadesse@gmail.com>
2021-10-12 15:38:01 -0700

    Read  text file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,7 +6,7 @@ contains methodto read a file
 
 def read_file(filename=""):
     """Reads a file with filename"""
-    new_file = open(filename)
+    new_file = open(filename, mode="r", encoding="utf-8")
     filetxt = new_file.read()
     new_file.close()
     filetxt
