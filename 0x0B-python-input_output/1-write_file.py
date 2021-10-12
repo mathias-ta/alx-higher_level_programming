6e7611b1fbe1665ff82da775e5a45e5110377766
mathias-tad <mathiasstadesse@gmail.com>
2021-10-12 15:54:37 -0700

    Write to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,5 +6,5 @@ contains method to write to a file
 
 def read_file(filename=""):
     """Write text to filename"""
-    with open(filename, mode="r", encoding="utf-8") as f_name:
+    with open(filename, mode="w", encoding="utf-8") as f_name:
         return(f_name.write(text))
