085543faeedf4570ebc414b2fb5ac5329c3e9e76
mathias-tad <mathiasstadesse@gmail.com>
2021-10-12 16:58:15 -0700

    Save all arguments to a file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,9 +9,10 @@ from sys import argv
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
+file_name = "add_item.json"
 try:
-    existing_content = load_from_json_file("add_item.json")
+    existing_content = load_from_json_file(file_name)
 except FileNotFoundError:
     existing_content = []
 
-save_to_json_file(existing_content + argv[1:], "add_item.json")
+save_to_json_file(existing_content + argv[1:], file_name)
