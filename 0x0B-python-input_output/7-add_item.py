2bea6e9375fb6bb161ba0ac88b6e75c0856cdd2e
mathias-tad <mathiasstadesse@gmail.com>
2021-10-12 16:49:22 -0700

    Save all arguments to a file

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+A scriptto add all arguments to a Python list and
+save them to a file
+"""
+
+
+from sys import argv
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+try:
+    existingcontent = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    ezistingcontent = []
+
+save_to_json_file(existing_content + argv[1:], "add_item.json")
