24cd425ca503d33d8a102360899dda11738b57b0
mathias-tad <mathiasstadesse@gmail.com>
2021-10-12 16:23:38 -0700

    load object from json file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+contains method to load object from JSON file
+"""
+
+
+def load_from_json_file(filename):
+    """Load object from JSON"""
+    import json
+    with open(filename, mode="r", encoding="utf-8") as f_name:
+        return json.load(my_obj, f_name)
