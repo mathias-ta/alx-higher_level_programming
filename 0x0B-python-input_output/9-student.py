d4e6b069e3333f96c7cc1365a9f8f4aa9e142483
mathias-tad <mathiasstadesse@gmail.com>
2021-10-12 17:15:43 -0700

    Student to json

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -5,7 +5,7 @@ a dictionary representation of student
 """
 
 
-class Student:
+class Student():
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
