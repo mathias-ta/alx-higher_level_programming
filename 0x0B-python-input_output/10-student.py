4da95f8fc5e89ebb807443f307a57c0404d6eaca
mathias-tad <mathiasstadesse@gmail.com>
2021-10-12 17:27:52 -0700

    Student to json with selection

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+contains class student and method to retrieves
+a dictionary representation of attributes of student
+"""
+
+
+class Student():
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is not None:
+            dicts = {}
+            for i in attrs:
+                if i in self.__dict__.keys():
+                    dicts[i] = self.__dict__[i]
+            return dicts
+        else:
+            return self.__dict__
