1dedd3300e66755d0cb4220d30c6660135c4529c
mathias-tad <mathiasstadesse@gmail.com>
2021-10-12 18:09:33 -0700

    pascal triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Contains method to create a pascal triangle
+"""
+
+
+def pascal_triangle(n):
+    ps_t = [[1]]
+    if n <= 0:
+        return []
+    elif n == 1:
+        return ps_t
+    else:
+        for i in range(n-1):
+        ps_t.append([a+b for a, b in zip([0] + ps_t[-1], ps_t[-1] + [0])])
+    return ps_t
