edc302435618cb32644b3460ae31d3329fb5f73d
mathias-tad <mathiasstadesse@gmail.com>
2021-10-18 13:58:28 -0700

    50 percent

--- 0x0C-python-almost_a_circle/main.py
+++ 0x0C-python-almost_a_circle/main.py
@@ -1,23 +1,14 @@
 #!/usr/bin/python3
-""" Doc """
+""" 1-main """
 from models.rectangle import Rectangle
 
 if __name__ == "__main__":
 
-    r1 = Rectangle(10, 10, 10, 10)
-    print(r1)
+    r1 = Rectangle(10, 2)
+    print(r1.id)
 
-    r1.update(89)
-    print(r1)
+    r2 = Rectangle(2, 10)
+    print(r2.id)
 
-    r1.update(89, 2)
-    print(r1)
-
-    r1.update(89, 2, 3)
-    print(r1)
-
-    r1.update(89, 2, 3, 4)
-    print(r1)
-
-    r1.update(89, 2, 3, 4, 5)
-    print(r1)
+    r3 = Rectangle(10, 2, 0, 0, 12)
+    print(r3.id)
